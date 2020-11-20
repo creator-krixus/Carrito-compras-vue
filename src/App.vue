01a63acbc33edc641c5aefa920ea140f34252970
Wilson <wilsonrueda2@gmail.com>
2020-11-20 11:54:22 -0500

    Practica con vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Carrito</h1>
+    <!-- {{productos}} -->
+    <!-- <pre>{{carrito}}</pre> -->
+    <hr>
+    <Carrito />
+    <div class="row">
+      <Card 
+        v-for="producto of productos" :key="producto.id" :producto="producto"
+      />  
+    </div>
+    
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {useStore} from 'vuex'
+import { computed, onMounted } from 'vue'
+import Card from './components/Card'
+import Carrito from './components/Carrito'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Card,
+      Carrito
+    },
+    setup(){
+      //Esto es para importar la tienda donde esta el fetchdata
+      const store = useStore()
+      onMounted(() => {
+        store.dispatch('fetchData')
+      })
+      // Se usa para mapear las propiedades computadas que se encuentran en setup y 
+      // poderlas mostrar en la app
+      const productos = computed(() => store.state.productos)
+      const carrito = computed(() => store.state.carrito)
+
+      return {productos, carrito}
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
